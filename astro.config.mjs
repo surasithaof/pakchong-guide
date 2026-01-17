6d8ba06b69f75ae4dc1585b5e98f872ec4e6db74
Surasith <surasith.kae@ascendcorp.com>
2026-01-18 02:01:42 +0700

    ci: deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://pakchong-guide.github.io',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'th',
