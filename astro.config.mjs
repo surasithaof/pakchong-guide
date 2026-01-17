e1f16cab288e7c13e606c0bfac11255d3fa86681
Surasith <surasith.kae@ascendcorp.com>
2026-01-18 02:04:36 +0700

    chore: fix url for gh page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://pakchong-guide.github.io',
+  site: "https://surasithaof.github.io",
+  base: "/pakchong-guide",
   integrations: [tailwind()],
   i18n: {
-    defaultLocale: 'th',
-    locales: ['th', 'en', 'zh', 'es', 'fr', 'it', 'ru'],
+    defaultLocale: "th",
+    locales: ["th", "en", "zh", "es", "fr", "it", "ru"],
   },
 });
